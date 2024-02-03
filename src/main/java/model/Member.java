2f91fbdc4a5ea6d3c39458ccdd35370f1b774174
Hwangjaegi <zzx960303@gmail.com>
2024-02-03 20:34:44 +0900

    1.제목 : Highbrix 커밋1 2.내용 : 헤더부분 샘플제작

---
+++ src/main/java/model/Member.java
@@ -0,0 +1,71 @@
+package model;
+
+import java.sql.Date;
+//회원정보 DTO
+public class Member {
+	private String id;
+	private String password;
+	private String address;
+	private String phone;
+	private String gender;
+	private int age;
+	private String email;
+	private Date reg_date;
+	private char del;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public char getDel() {
+		return del;
+	}
+	public void setDel(char del) {
+		this.del = del;
+	}
+	
+	
+}
